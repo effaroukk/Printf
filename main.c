4f37784f7c3a85823ca99a2bfc07a0ad7f0e33bc
effaroukk <efqay1@gmail.com>
2023-09-18 16:18:23 +0100

    update

--- main.c
+++ main.c
@@ -41,4 +41,16 @@ int main(void)
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("%b\n", 98);
+	return (0);
 }

94f0181eaa4ac6c66d6fd22d06dcf137cfc545a4
effaroukk <efqay1@gmail.com>
2023-09-21 02:47:17 +0100

    updated

--- main.h
+++ main.h
@@ -77,7 +77,7 @@ int print_pointer(va_list types, char buffer[],
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_flags(const char *format, int *i);
-int get_size(const char *format, int *k);
+int get_size(const char *format, int *z);
 
 /* To print non printable characters */
 int print_non_printable(va_list types, char buffer[],

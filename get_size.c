5b08bb9ca931cb9ded74d38e876c20a3fcf29cbe
effaroukk <efqay1@gmail.com>
2023-09-21 02:05:04 +0100

    updated

--- get_size.c
+++ get_size.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
- * get_size - Calculate size modifier for the next format specifier
- * @format: The format string
+ * get_size - Extract size modifier from a format string
+ * @format: The formatted string
  * @position: Pointer to the current position in the format string
  *
- * Return: Size modifier (0 for none, 'l' for long, 'h' for short)
+ * Return: Size modifier ('l' for long, 'h' for short, 0 for none)
  */
-int get_size(const char *format, size_t *position)
+int get_size(const char *format, int *position)
 {
 	int size_modifier = 0;
+	int ef_position = *position + 1;
 
-	if (format[*position] == 'l')
+	if (format[ef_position] == 'l')
 	{
-		size_modifier = 'l';
-		(*position)++;
+	size_modifier = 'l';
 	}
-	else if (format[*position] == 'h')
+	else if (format[ef_position] == 'h')
 	{
-		size_modifier = 'h';
-		(*position)++;
+	size_modifier = 'h';
+	}
+	if (size_modifier == 0)
+	{
+		*position = ef_position - 1;
+	}
+	else
+	{
+	*position = ef_position;
 	}
 
 	return (size_modifier);
